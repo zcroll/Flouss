9ae3594b80066073db68374ed417455830497346
zcroll <hamid2001.nado@gmail.com>
2024-08-22 23:44:26 +0100

    Comment out unused baseUri function
    The `baseUri` function within the share method of `HandleInertiaRequests.php` has been commented out as it is not currently in use. This change helps improve code clarity and maintainability.

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,9 +36,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-
-            'baseUri' => fn ($request) => $request->url()
-
+//            'baseUri' => fn ($request) => $request->url()
         ]);
     }
 }
