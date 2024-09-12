7c4bdec2c0fc4a2e12242e978023a23c1705a8dc
zcroll <hamid2001.nado@gmail.com>
2024-09-12 19:10:52 +0100

    changing the vue of the personality

--- resources/js/Pages/Result/Personality.vue
+++ resources/js/Pages/Result/Personality.vue
@@ -5,67 +5,91 @@
         <header class="flex justify-between items-center mb-8">
           <h1 class="text-3xl font-bold m-0 text-purple-800">{{ ArchetypeData.name }}'s Personality Report</h1>
           <div class="flex space-x-3">
-            <button class="bg-blue-200 border-none text-blue-800 py-2 px-4 rounded-lg cursor-pointer transition duration-300 ease-in-out hover:bg-blue-300 text-sm">
+            <button class="relative isolate inline-flex items-center justify-center gap-x-2 rounded-lg border text-base/6 font-semibold px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6 focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500 data-[disabled]:opacity-50 [&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-5 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:sm:my-1 [&>[data-slot=icon]]:sm:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText] border-zinc-950/10 text-zinc-950 data-[active]:bg-zinc-950/[2.5%] data-[hover]:bg-zinc-950/[2.5%] dark:border-white/15 dark:text-white dark:[--btn-bg:transparent] dark:data-[active]:bg-white/5 dark:data-[hover]:bg-white/5 [--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)]">
               <i class="fas fa-print mr-2"></i> Print
             </button>
-            <button class="bg-green-200 border-none text-green-800 py-2 px-4 rounded-lg cursor-pointer transition duration-300 ease-in-out hover:bg-green-300 text-sm">
+            <button class="relative isolate inline-flex items-center justify-center gap-x-2 rounded-lg border text-base/6 font-semibold px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6 focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500 data-[disabled]:opacity-50 [&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-5 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:sm:my-1 [&>[data-slot=icon]]:sm:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText] border-zinc-950/10 text-zinc-950 data-[active]:bg-zinc-950/[2.5%] data-[hover]:bg-zinc-950/[2.5%] dark:border-white/15 dark:text-white dark:[--btn-bg:transparent] dark:data-[active]:bg-white/5 dark:data-[hover]:bg-white/5 [--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)]">
               <i class="fas fa-share-alt mr-2"></i> Share
             </button>
           </div>
         </header>
-        
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-          <div v-for="(card, index) in cards" :key="index" class="cursor-pointer bg-gray-50 rounded-lg flex flex-col p-5 transition duration-300 ease-in-out transform hover:translate-y-[-3px] hover:shadow-md text-gray-800">
-            <p class="mt-0 text-base leading-6 mb-4 flex-grow">{{ card.content }}</p>
-            <button class="bg-transparent border-none text-blue-800 py-2 text-left cursor-pointer text-sm font-semibold transition duration-300 ease-in-out hover:text-blue-600">{{ card.buttonText }}</button>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+          <div v-for="(card, index) in cards" :key="index" class="relative h-full w-full rounded-xl bg-sky-100 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.5),0px_2px_2px_0px_rgba(0,0,0,0.5)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
+            <div class="grid h-full w-full justify-items-center overflow-hidden place-items-center p-4 py-8 sm:p-8 lg:p-12">
+              <p class="mt-0 text-base leading-6 mb-4 flex-grow font-inter text-zinc-700 dark:text-zinc-300">{{ card.content.substring(0, 150) }}...</p>
+              <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 sm:relative sm:bottom-0 sm:left-0 sm:transform-none sm:mt-4">
+                <button @click="scrollToSection(card.sectionId)" class="relative isolate mb-3 inline-flex items-center justify-center gap-x-2 rounded-lg border text-sm font-semibold px-3 py-2 sm:px-2 sm:py-1 sm:text-xs focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500 data-[disabled]:opacity-50 [&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-4 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:sm:my-1 [&>[data-slot=icon]]:sm:size-3 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText] border-transparent bg-[--btn-border] dark:bg-[--btn-bg] before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-[--btn-bg] before:shadow dark:before:hidden dark:border-white/5 after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)] after:shadow-[shadow:inset_0_1px_theme(colors.white/15%)] after:data-[active]:bg-[--btn-hover-overlay] after:data-[hover]:bg-[--btn-hover-overlay] dark:after:-inset-px dark:after:rounded-lg before:data-[disabled]:shadow-none after:data-[disabled]:shadow-none text-white [--btn-bg:theme(colors.zinc.900)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.white/10%)] dark:text-white dark:[--btn-bg:theme(colors.zinc.600)] dark:[--btn-hover-overlay:theme(colors.white/5%)] [--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.300)] data-[hover]:[--btn-icon:theme(colors.zinc.300)] font-inter">
+                  <span class="hidden sm:inline">{{ card.buttonText }}</span>
+                  <span class="sm:hidden text-xs">{{ card.buttonText }}</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div class="mb-8">
-          <h1 class="text-3xl font-bold mb-4 text-purple-800">You are a {{ ArchetypeData.name }}</h1>
+        <div class="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:max-w-4xl lg:px-12">
+          <h2 id="personality-profile" class="inline-flex items-center rounded-full px-4 py-1 text-blue-600 ring-1 ring-inset ring-blue-600">
+            <span class="font-mono text-sm" aria-hidden="true">01</span>
+            <span class="ml-3 h-3.5 w-px bg-blue-600/20"></span>
+            <span class="ml-3 text-base font-medium tracking-tight">Your Personality Profile</span>
+          </h2>
+
+          <h1 class="mt-8 font-display text-4xl font-bold tracking-tight text-slate-900">You are a {{ ArchetypeData.name }}</h1>
           
-          <p class="text-gray-700 text-base leading-6 mb-3">
+          <p class="mt-4 text-lg tracking-tight text-slate-700">
             Your strongest trait is {{ ArchetypeData.primary_trait }}, and your second strongest is {{ ArchetypeData.secondary_trait }}, which makes you a {{ ArchetypeData.name }}.
           </p>
           
-          <p class="text-gray-700 text-base leading-6 mb-3">
+          <p class="mt-4 text-lg tracking-tight text-slate-700">
             {{ ArchetypeData.description }}
           </p>
 
-          <div class="mb-8">
+          <div class="mt-16">
             <!-- Replace this with your actual chart component -->
             <img src="path_to_your_chart_image" alt="Personality Traits Chart" class="max-w-full h-auto mx-auto">
           </div>
 
-          <h1 class="text-3xl font-bold mb-4 text-purple-800">Skills You Can Focus On</h1>
-          <p class="text-gray-700 text-base leading-6 mb-8">
+          <h2 id="strengths-and-growth" class="mt-16 inline-flex items-center rounded-full px-4 py-1 text-blue-600 ring-1 ring-inset ring-blue-600">
+            <span class="font-mono text-sm" aria-hidden="true">02</span>
+            <span class="ml-3 h-3.5 w-px bg-blue-600/20"></span>
+            <span class="ml-3 text-base font-medium tracking-tight">Your Strengths and Areas for Growth</span>
+          </h2>
+
+          <h3 class="mt-8 font-display text-3xl font-bold tracking-tight text-slate-900">Skills You Can Focus On</h3>
+          <p class="mt-4 text-lg tracking-tight text-slate-700">
             {{ ArchetypeData.strengths }}
           </p>
 
-          <h1 class="text-3xl font-bold mb-4 text-purple-800">Tendencies To Be Careful Of</h1>
-          <p class="text-gray-700 text-base leading-6 mb-8">
+          <h3 class="mt-8 font-display text-3xl font-bold tracking-tight text-slate-900">Tendencies To Be Careful Of</h3>
+          <p class="mt-4 text-lg tracking-tight text-slate-700">
             {{ ArchetypeData.weaknesses }}
           </p>
-          <h1 class="text-3xl font-bold mb-4 text-purple-800">Your Working Style</h1>
-          <p class="text-gray-700 text-base leading-6 mb-8">
+
+          <h2 id="working-style" class="mt-16 inline-flex items-center rounded-full px-4 py-1 text-blue-600 ring-1 ring-inset ring-blue-600">
+            <span class="font-mono text-sm" aria-hidden="true">03</span>
+            <span class="ml-3 h-3.5 w-px bg-blue-600/20"></span>
+            <span class="ml-3 text-base font-medium tracking-tight">Your Working Style</span>
+          </h2>
+
+          <h3 class="mt-8 font-display text-3xl font-bold tracking-tight text-slate-900">Your Working Style</h3>
+          <p class="mt-4 text-lg tracking-tight text-slate-700">
             {{ ArchetypeData.personality_paragraph }}
           </p>
 
-          <div class="mb-8">
-            <div v-for="(category, index) in groupedInsights" :key="index" class="mb-2 border-b border-gray-200 last:border-b-0">
-              <div class="flex items-center py-3 cursor-pointer" @click="toggleInsight(index)">
-                <div class="bg-gray-200 rounded-full p-2 mr-3">
-                  <!-- <img :src="require(`~/resources/images/insights/${getIcon(category.category)}.svg`)"  class="w-6 h-6"> -->
-                </div>
-                <h3 class="text-lg font-semibold m-0 text-gray-800 flex-grow">{{ category.category }}</h3>
-                <span class="text-sm text-gray-500">{{ category.insights.length }} Insights Available</span>
-                <i class="fas fa-chevron-right ml-3 text-gray-400 transition-transform duration-300 ease-in-out" :class="{ 'rotate-90': category.isOpen }"></i>
-              </div>
-              <ul v-if="category.isOpen" class="list-none pl-12 py-3">
-                <li v-for="(insight, insightIndex) in category.insights" :key="insightIndex" class="text-gray-700 text-base leading-6 mb-2">
-                  {{ insight }}
+          <h2 id="detailed-insights" class="mt-16 inline-flex items-center rounded-full px-4 py-1 text-blue-600 ring-1 ring-inset ring-blue-600">
+            <span class="font-mono text-sm" aria-hidden="true">04</span>
+            <span class="ml-3 h-3.5 w-px bg-blue-600/20"></span>
+            <span class="ml-3 text-base font-medium tracking-tight">Detailed Insights</span>
+          </h2>
+
+          <div class="mt-8">
+            <div v-for="(category, index) in groupedInsights" :key="index" class="mt-8">
+              <h3 class="font-display text-3xl font-bold tracking-tight text-slate-900">{{ category.category }}</h3>
+              <ol role="list" class="mt-8 divide-y divide-slate-300/30 rounded-2xl bg-slate-50 px-6 py-3 text-base tracking-tight sm:px-8 sm:py-7">
+                <li v-for="(insight, insightIndex) in category.insights" :key="insightIndex" class="flex justify-between py-3">
+                  <span class="font-medium text-slate-900">{{ insight }}</span>
                 </li>
-              </ul>
+              </ol>
             </div>
           </div>
         </div>
@@ -98,7 +122,6 @@ export default {
           grouped[insight.insight_category] = {
             category: insight.insight_category,
             insights: [],
-            isOpen: false
           };
         }
         grouped[insight.insight_category].insights.push(insight.insight);
@@ -111,18 +134,20 @@ export default {
       cards: [
         {
           content: `You are a ${this.ArchetypeData.name}. Your strongest trait is ${this.ArchetypeData.primary_trait}, and second strongest trait is ${this.ArchetypeData.secondary_trait}.`,
-          buttonText: `LEARN ABOUT ${this.ArchetypeData.name.toUpperCase()}S`
+          buttonText: `learn about your ${this.ArchetypeData.name.toLowerCase()}s`,
+          sectionId: 'personality-profile'
         },
         {
-          content: "You love being creative with your ideas. You have an active imagination and like to interpret the world around you before expressing your findings to others.",
-          buttonText: "LEARN ABOUT YOUR SKILLS"
+          content: this.ArchetypeData.strengths,
+          buttonText: "learn about your skills",
+          sectionId: 'strengths-and-growth'
         },
         {
-          content: "You likely have a general proclivity for self reflection, which makes you particularly receptive to your environment and your place in it.",
-          buttonText: "LEARN ABOUT YOUR STRENGTHS"
+          content: this.ArchetypeData.personality_paragraph,
+          buttonText: "learn about your strengths",
+          sectionId: 'working-style'
         }
       ],
-      groupedInsights: [],
       svgChart: '',
       randomScores: {
         Realistic: Math.random(),
@@ -133,9 +158,6 @@ export default {
         Conventional: Math.random()
       }
     }
-  },
-  created() {
-    this.groupInsights();
   },
   mounted() {
     this.createRadarChart();
@@ -150,23 +172,6 @@ export default {
         case 'ideal work environment': return 'ideal-work-environment';
         default: return 'default-icon';
       }
-    },
-    groupInsights() {
-      const grouped = {};
-      this.Insights.forEach((insight) => {
-        if (!grouped[insight.insight_category]) {
-          grouped[insight.insight_category] = {
-            category: insight.insight_category,
-            insights: [],
-            isOpen: false
-          };
-        }
-        grouped[insight.insight_category].insights.push(insight.insight);
-      });
-      this.groupedInsights = Object.values(grouped);
-    },
-    toggleInsight(index) {
-      this.groupedInsights[index].isOpen = !this.groupedInsights[index].isOpen;
     },
     createRadarChart() {
       const chart = radar({
@@ -203,16 +208,22 @@ export default {
         }),
       });
 
-      this.svgChart = `
-        <svg version="1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="300" height="250">
-          <style>
-            .axis { stroke: #888; stroke-width: 1; }
-            .shape { fill: rgba(168, 8, 247, 0.5); stroke: rgba(168, 85, 247, 1); stroke-width: 2; }
-          </style>
-          <rect width="100%" height="100%" fill="#f8f8f8" />
-          ${stringify(chart)}
-        </svg>
-      `;
+      // this.svgChart = `
+      //   <svg version="1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="300" height="250">
+      //     <style>
+      //       .axis { stroke: #888; stroke-width: 1; }
+      //       .shape { fill: rgba(168, 8, 247, 0.5); stroke: rgba(168, 85, 247, 1); stroke-width: 2; }
+      //     </style>
+      //     <rect width="100%" height="100%" fill="#f8f8f8" />
+      //     ${stringify(chart)}
+      //   </svg>
+      // `;
+    },
+    scrollToSection(sectionId) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
     }
   }
 }
