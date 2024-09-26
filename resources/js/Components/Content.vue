3fd06c10be2f61a8f24c189e27d9a3cf8335a3e6
YIPA-01 <yahyanireda@gmail.com>
2024-09-26 18:50:57 +0100

    landing page color fix

--- resources/js/Components/Content.vue
+++ resources/js/Components/Content.vue
@@ -3,12 +3,12 @@
     <div class="grid-container">
       <div class="grid-x">
         <div class="large-4 medium-12 small-12 cell">
-          <h2 class="why-ch__title before-fade-in fade-in">
+          <h2 class="why-ch__title before-fade-in fade-in ">
             {{ __('welcome.unrivaled_testing') }}
           </h2>
           <ul class="accordion nested enable-active-medium-up">
             <li v-for="(item, index) in accordionItems" :key="index" class="accordion-item before-fade-in fade-in" :class="{ 'is-active': activeIndex === index }">
-              <span class="accordion-title" @click="toggleAccordion(index)">
+              <span class="accordion-title text-gray-400" @click="toggleAccordion(index)">
                 {{ __(`welcome.accordion_items.${item.key}.title`) }}
               </span>
               <div class="accordion-content" :style="{ display: activeIndex === index ? 'block' : 'none' }">
