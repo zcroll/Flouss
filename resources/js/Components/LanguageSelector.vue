5b7dca915266a19aa719cde8c26188a1cd3ea618
YIPA-01 <yahyanireda@gmail.com>
2024-09-27 03:19:15 +0100

    language selector

--- resources/js/Components/LanguageSelector.vue
+++ resources/js/Components/LanguageSelector.vue
@@ -1,40 +1,80 @@
 <template>
-  <select 
-    name="language" 
-    id="language" 
-    v-on:change="router.post(route('language.switch', { language: $event.target.value }))"
-    class="bg-transparent text-white border border-slate-500 focus:border-green-400 focus:ring-2 focus:ring-green-400 rounded-md px-2 py-1 text-sm mr-4"
-  >
-    <option 
-      :value="language.value" 
-      v-for="language in languages"
-      :key="language.value" 
-      :selected="language.value === selectedLanguage"
-      class="bg-[#0a1e2e] text-white"
+  <ol class="relative" @mouseleave="startCloseTimer">
+    <label
+      class="flex gap-2 items-center py-1.5 cursor-pointer lowercase text-white"
+      tabindex="0"
+      @mouseenter="showDropdown = true"
     >
-      {{ language.value === 'en' ? 'En' : 'Fr' }}
-    </option>
-  </select>
+      <img class="h-5" :src="getFlagSrc(selectedLanguage)" :alt="`${selectedLanguage} flag`" />
+      <svg height="16" width="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42Z" fill="currentColor" />
+      </svg>
+    </label>
+    <ul
+      v-show="showDropdown"
+      class="bg-white flex flex-col gap-2 p-2 rounded-box shadow text-black absolute z-10 w-28 end-1"
+      tabindex="0"
+      @mouseenter="cancelCloseTimer"
+      @mouseleave="startCloseTimer"
+    >
+      <li v-for="language in languages" :key="language.value">
+        <a
+          class="hover:text-primary duration-300 transition ease-in-out flex gap-1 flex-row items-center px-3 py-1"
+          @click.prevent="switchLanguage(language.value)"
+          href="#"
+          rel="nofollow"
+        >
+          <img class="h-4" :src="getFlagSrc(language.value)" :alt="`${language.value} flag`" />
+          <span>{{ getLanguageName(language.value) }}</span>
+        </a>
+      </li>
+    </ul>
+  </ol>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import { router } from '@inertiajs/vue3';
 
-export default {
-  props: {
-    languages: {
-      type: Array,
-      required: true,
-    },
-    selectedLanguage: {
-      type: String,
-      required: true,
-    },
+const props = defineProps({
+  languages: {
+    type: Array,
+    required: true,
   },
-  setup() {
-    return {
-      router,
-    };
+  selectedLanguage: {
+    type: String,
+    required: true,
   },
+});
+
+const showDropdown = ref(false);
+let closeTimer = null;
+
+const getFlagSrc = (languageCode) => {
+  return `https://www.markoub.ma/Content/front/new/img/country-flags/${languageCode}.svg`;
+};
+
+const getLanguageName = (languageCode) => {
+  return languageCode === 'en' ? 'English' : 'Français';
+};
+
+const switchLanguage = (language) => {
+  router.post(route('language.switch', { language }));
+};
+
+const startCloseTimer = () => {
+  closeTimer = setTimeout(() => {
+    showDropdown.value = false;
+  }, 300);
+};
+
+const cancelCloseTimer = () => {
+  if (closeTimer) {
+    clearTimeout(closeTimer);
+  }
 };
 </script>
+
+<style scoped>
+/* You can add scoped styles here if needed */
+</style>
