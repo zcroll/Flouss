0d74f201777ef79c78bd76cc6897ce86cfd03ade
zcroll <hamid2001.nado@gmail.com>
2024-11-17 22:44:30 +0100

    adding the small screen size to the login mr reda !!!!!

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -52,7 +52,7 @@ const submit = () => {
             {{ error }}
         </div>
 
-        <form @submit.prevent="submit" class="space-y-8">
+        <form @submit.prevent="submit" class="space-y-6 max-w-sm mx-auto">
             <div>
                 <InputLabel for="email" value="Email" />
                 <TextInput
@@ -89,14 +89,14 @@ const submit = () => {
                 </Link>
             </div>
 
-            <div>
-                <PrimaryButton class="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 " :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+            <div class="mt-6">
+                <PrimaryButton class="flex w-full justify-center rounded-md px-4 py-2 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Sign in
                 </PrimaryButton>
             </div>
         </form>
 
-        <div class="relative mt-10">
+        <div class="relative mt-10 max-w-sm mx-auto">
             <div class="absolute inset-0 flex items-center" aria-hidden="true">
                 <div class="w-full border-t border-gray-200" />
             </div>
@@ -104,8 +104,9 @@ const submit = () => {
                 <span class="bg-white px-6 text-gray-900">Or continue with</span>
             </div>
         </div>
-        <div class="mt-6 flex justify-center">
-            <a href="/auth/google" class="flex items-center justify-center gap-3 rounded-md bg-white px-2 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent">
+        
+        <div class="mt-6 max-w-sm mx-auto">
+            <a href="/auth/google" class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent">
               <svg class="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
                 <path d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z" fill="#EA4335" />
                 <path d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z" fill="#4285F4" />
