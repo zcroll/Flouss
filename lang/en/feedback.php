835ffc27036a5823d613808aef9ebc85d9c3c6a1
YIPA-01 <yahyanireda@gmail.com>
2024-11-22 21:30:43 +0100

    translation slip

--- lang/en/feedback.php
+++ lang/en/feedback.php
@@ -4,4 +4,8 @@ return [
     'help_improve' => 'Help us improve',
     'thank_you' => 'Thank you for your feedback!',
     'tell_us' => 'Tell us what you thought',
+    'submit' => 'Submit',
+    'submitting' => 'Submitting...',
+    'thanks' => 'Thanks for your feedback!',
+    'thanks_message' => 'We\'ll use your feedback to make the site even better.',
 ];
--- lang/fr/feedback.php
+++ lang/fr/feedback.php
@@ -4,4 +4,8 @@ return [
     'help_improve' => 'Aidez-nous à nous améliorer',
     'thank_you' => 'Merci pour votre feedback !',
     'tell_us' => 'Dites-nous ce que vous avez pensé',
+    'submit' => 'Envoyer',
+    'submitting' => 'Soumission en cours...',
+    'thanks' => 'Merci pour votre feedback !',
+    'thanks_message' => 'Nous utiliserons votre feedback pour rendre le site encore meilleur.',
 ];
